1f3dae077e1a6e77a4b5664d5c9ab103ed224564
LJN <g123nn@hotmail.com>
2016-07-25 10:14:08 +0800

    Remove AppInsight

--- RandomMaster/App.xaml.cs
+++ RandomMaster/App.xaml.cs
@@ -37,9 +37,6 @@ namespace RandomMaster
 
 		public App()
 		{
-			Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-				Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-				Microsoft.ApplicationInsights.WindowsCollectors.Session);
 			InitializeComponent();
 		}
 
